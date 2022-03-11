da40dd5778e288422154e294c2b3dfa84f2cea54
s5779870b <s5779870b@studenti.itisavogadro.it>
2022-03-11 14:46:25 +0100

    modifica header

---
+++ src/views/teachers/MyClasses.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+// import MyCourseCard from '../components/Course/MyCourseCard.vue'
+/* import { subscribeService as ss } from '../servises/subscribe.service'
+import { coursesService as cs } from '../servises/course.services'
+import store from '@/store/index' */
+
+export default {
+  name: 'mycourses',
+  data: function () {
+    return {}
+  },
+  components: {},
+  mounted () {},
+  methods: {}
+}
+</script>
