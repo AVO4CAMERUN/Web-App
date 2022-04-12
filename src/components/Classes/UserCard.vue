f26837f00994ff16224ed81e091ae7348f9f2bf7
samuuuuuuuuu <98238657+samuuuuuuuuu@users.noreply.github.com>
2022-04-13 00:44:31 +0200

    ho rotto tutto
    pietro

--- src/components/Classes/UserCard.vue
+++ src/components/Classes/UserCard.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="relative cursor-pointer w-full min-w-[415px] h-[100px] bg-white p-2 outline outline-[1px] outline-gray-200 rounded-xl flex items-center gap-4 select-none transition-shadow hover:shadow-md hover:shadow-slate-300">
-      <div class="h-full aspect-square border-2 border-transparent rounded-full overflow-hidden">
-          <img class="w-full h-full object-contain" src="https://demo.themesberg.com/windster/images/users/neil-sims.png" alt="Neil Sims avatar" />
+  <div class="relative cursor-pointer w-full min-w-[415px] h-[100px] bg-white p-4 outline outline-[1px] outline-gray-200 rounded-xl flex justify-between items-center select-none transition-shadow hover:shadow-md hover:shadow-slate-300">
+      <div class="h-full aspect-square border-transparent rounded-full overflow-hidden">
+          <img class="w-full h-full object-cover" src="https://demo.themesberg.com/windster/images/users/neil-sims.png" alt="profile img" />
       </div>
       <div class="flex flex-col gap-2">
           <span class="text-base font-semibold text-gray-900">{{lastname}} {{firstname}}</span>
           <span class="text-sm font-normal text-gray-500">{{email}}</span>
       </div>
-      <div class="absolute right-6">
-        <p class="flex items-center gap-2">
-          <span class="bg-green-400 h-2 block aspect-square rounded-full" />
-          Active
-        </p>
+      <div class="flex items-center gap-2">
+        <span class="bg-green-400 h-2 block aspect-square rounded-full" />
+        Online
       </div>
   </div>
 </template>
